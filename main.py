183374cf9c73e54ec2575324837aac722713371b
Glen <glen_chue@u.nus.edu>
2022-02-22 16:59:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -26,6 +26,9 @@ print(
     f'[6] + GC Content in Subsection k=5: {gc_content_subsec(DNAStr, k=5)}\n')
 print(
     f'[7] + Aminoacids Sequence from DNA: {translate_seq(DNAStr, 0)}\n')
-
 print(
     f'[8] + Codon frequency (L): {codon_usage(DNAStr, "L")}\n')
+print('[9] + Reading_frames:')
+for frame in gen_reading_frames(DNAStr):
+    print(frame)
+
